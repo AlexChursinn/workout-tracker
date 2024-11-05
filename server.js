e4ed3bfde3bb4506e74fc17262b5d21a42351cf1
Alex <alex@MacBook-Air-Alex.local>
2024-11-05 18:33:59 +0500

    Configure server to use dynamic port for Render

--- server.js
+++ server.js
@@ -45,7 +45,7 @@ app.post('/api/workouts', (req, res) => {
   res.status(201).json(newWorkout);
 });
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
