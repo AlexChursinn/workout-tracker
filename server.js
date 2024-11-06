a6e5c23cfb51e2de35d2f065f585fbdfc3717afc
Alex Chursin <alex-chursin@ay-money.kz>
2024-11-06 21:39:43 +0500

    Initial commit 2

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const cors = require('cors');
 const app = express();
 
 app.use(cors({
-  origin: ['http://localhost:3000', 'https://workout-tracker-beta-rose.vercel.app'], // Добавьте URL вашего фронтенда
+  origin: '*', // Временно разрешить все источники для тестирования
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   credentials: true,
 }));
