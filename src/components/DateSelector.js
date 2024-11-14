abab6a8643b63a3dd5b5a9011d702d745a201126
Alex Chursin <alex-chursin@ay-money.kz>
2024-11-14 10:43:03 +0500

    Стили для календаря

--- src/components/DateSelector.js
+++ src/components/DateSelector.js
@@ -4,6 +4,7 @@ import DatePicker from 'react-datepicker';
 import 'react-datepicker/dist/react-datepicker.css';
 import { registerLocale } from 'react-datepicker';
 import ru from 'date-fns/locale/ru';
+import './DateSelector.css'; // Подключаем CSS для кастомных стилей
 
 registerLocale('ru', ru);
 
@@ -19,7 +20,7 @@ const DateSelector = ({ selectedDate, onDateSelect, filledDates }) => {
     return (
       <div
         style={{
-          backgroundColor: isFilled ? '#4CAF50' : 'transparent',
+          backgroundColor: isFilled ? '#000' : 'transparent',
           color: isFilled ? 'white' : '',
           borderRadius: isFilled ? '50%' : '',
           padding: '5px'
@@ -32,7 +33,7 @@ const DateSelector = ({ selectedDate, onDateSelect, filledDates }) => {
 
   return (
     <div>
-      <h2 onClick={() => setOpenCalendar(!openCalendar)}>
+      <h2 className="dateDisplay" onClick={() => setOpenCalendar(!openCalendar)}>
         {selectedDate.toLocaleDateString()}
       </h2>
       {openCalendar && (
@@ -46,6 +47,7 @@ const DateSelector = ({ selectedDate, onDateSelect, filledDates }) => {
           renderDayContents={renderDayContents}
           locale="ru"
           dateFormat="dd.MM.yyyy"
+          calendarClassName="custom-datepicker" // Кастомный класс
         />
       )}
     </div>
