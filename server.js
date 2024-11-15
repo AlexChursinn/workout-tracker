a0ffd150e63fe6e71b4ecd07c5da831a7aa785bf
Alex Chursin <alex-chursin@ay-money.kz>
2024-11-15 12:53:47 +0500

    Фикс Render 1

--- server.js
+++ server.js
@@ -15,9 +15,22 @@ app.use(cors({
   credentials: true,
 }));
 
+// Обработка preflight-запросов для всех маршрутов
+app.options('*', cors());
+
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", req.headers.origin);
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  if (req.method === 'OPTIONS') {
+    return res.status(200).end();
+  }
+  next();
+});
+
 app.use(express.json());
 
-const SECRET_KEY = 'your_secret_key';
+const SECRET_KEY = process.env.SECRET_KEY || 'your_secret_key';
 const dbFile = 'db.json';
 
 // Функция чтения базы данных
